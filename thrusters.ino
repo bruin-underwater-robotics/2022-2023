0d726689ac348480ca2d3d18ceb588cc974505fa
vanisharmag <67726421+vanisharmag@users.noreply.github.com>
2022-11-19 14:02:17 -0800

    updated spin direction

--- thrusters.ino
+++ thrusters.ino
@@ -14,19 +14,17 @@ byte servo_pins = 6; // signal pin for the ESC. needs 6 pins next to each other
 Servo servos[6];
 
 // vectors for controlling linear movement ? but now im not sure how to go down 
-float top_right[] = {-.25, -.25, 0}; // jank rn but can expiremtn 
-float top_left[] = {-.25, .25, 0};
-float bottom_right[] = {.25, -.25, 0};
-float bottom_left[] = {.25, .25, 0};
+float top_right[] = {-.25, .25, 0}; // jank rn but can expiremtn 
+float top_left[] = {.25, -.25, 0};
+float bottom_right[] = {.25, .25, 0};
+float bottom_left[] = {-.25, -.25, 0};
 float right[] = {0, 0, .5};
-float left[] = {0, 0, .5};
+float left[] = {0, 0, -.5};
 
 double pwm_x=0;
 double pwm_y=0;
 double pwm_z=0; 
 double pwm = 0;
-
-#define PI 3.14159
 
 // depends on how thrusters are plugged in 
 float* thrusters[] = {top_right, top_left, bottom_right, bottom_left, right, left};
@@ -38,24 +36,16 @@ void handler( const geometry_msgs::Twist& msg ) {
     pwm_x = thrusters[i][0] * msg.linear.x; 
     pwm_y = thrusters[i][1] * msg.linear.y; 
     pwm_z = thrusters[i][2] * msg.linear.z; 
+     
+    pwm = map(long(100 * (pwm_x + pwm_y + pwm_z)), -50, 50, 0, 180); // 1100 to 1900 for the thrusters
 
-    pwm = map(long(100 * (pwm_x + pwm_y + pwm_z)), -50, 50, 0, 180); 
-
-    // pwm_x = map((thrusters[i][0]*msg.linear.x), -1, 1, 0, 60);
-    // pwm_y = map((thrusters[i][1]*msg.linear.y), -1, 1, 0, 60);
-    // pwm_z = map((thrusters[i][2]*msg.linear.z), -1, 1, 0, 60);
-    //pw = atan(msg.linear.x / msg.linear.y) * (180.0 / PI);
-    servos[i].write( pwm /*pwm_x + pwmmy + pwm_z*/); // Send signal to ESC.
-    // digitalWrite(LED_BUILTIN, HIGH);
-    // delay(100);
-    // digitalWrite(LED_BUILTIN, LOW);
+    servos[i].write( pwm ); // Send signal to ESC.
   }
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &handler);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   nh.initNode(); 
   nh.subscribe(sub); 
 
@@ -63,13 +53,12 @@ void setup() {
     servos[i].attach(i+servo_pins); 
   }
   for(int i = 0; i<6; i++){
-    servos[i].write(90); 
+    servos[i].write(90); // 1500 for the thrusters 
   }
   delay(7000); // delay to allow the ESC to recognize the stopped signal.
 }
 
 void loop(){
-  digitalWrite(LED_BUILTIN, LOW);
   nh.spinOnce();
 }
 
