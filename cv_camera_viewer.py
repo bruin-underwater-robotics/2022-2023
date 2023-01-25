ae29d0f64d2ce1f3d1046cf3389f351c6e6d6da1
--replace-all <stanley.wei.04@gmail.com>
2023-01-24 16:31:35 -0800

    Laptop video viewer for cv_camera [from Pi]

---
+++ cv_camera_viewer.py
@@ -0,0 +1,21 @@
+import cv2
+import rospy
+from sensor_msgs.msg import Image
+import numpy as np
+from cv_bridge import CvBridge
+
+bridge = CvBridge()
+
+def callback(image):
+        cv_image = bridge.imgmsg_to_cv2(image, desired_encoding='rgb8')
+        cv2.imshow("test", cv_image)
+        cv2.waitKey(1)
+
+def listener():
+        rospy.init_node("test", anonymous=True)
+        rospy.Subscriber("cv_camera/image_raw", Image, callback)
+        rospy.spin()
+
+if __name__ == '__main__':
+        listener()
+        
