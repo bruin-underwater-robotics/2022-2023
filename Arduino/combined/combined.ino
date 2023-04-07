30f331ca93a497eb3843ec7493c898e528ed78f1
AX90L0TL <38091876+ax90l0tl@users.noreply.github.com>
2023-04-07 00:43:31 -0700

    pwm change

--- Arduino/combined/combined.ino
+++ Arduino/combined/combined.ino
@@ -1,33 +1,52 @@
-
-#include <ros.h>
-#include <rov/cmd_thruster.h>
-#include <rov/imu.h>
+  // Messages and Libraries
 #include <Servo.h>
-#include <geometry_msgs/Twist.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+//ROS
+#include <ros.h>
+//ROS message files
+#include <rov/cmd_thruster.h>
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/Int8.h>
 #include <Adafruit_BNO08x.h>
 
-// For SPI mode, we need a CS pin
-#define BNO08X_CS 10
-#define BNO08X_INT 9
-
+/*********************************
+------------IMU-Setup-------------
+*********************************/
 // For SPI mode, we also need a RESET 
-//#define BNO08X_RESET 5
 // but not for I2C or UART
 #define BNO08X_RESET -1
 
 Adafruit_BNO08x  bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 
+/*********************************
+---------Temp-Sensor-Setup--------
+*********************************/
+// Data wire is plugged into port 2 on the Arduino
+//#define ONE_WIRE_BUS 54
+//// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+//OneWire oneWire(ONE_WIRE_BUS);
+//// Pass our oneWire reference to Dallas Temperature. 
+//DallasTemperature sensors(&oneWire);
+//// arrays to hold device address
+//DeviceAddress insideThermometer;
+
+/*********************************
+--------Motor-Button-Setup---------
+*********************************/
+//Motor and Button Setup
 Servo motor[6];
-ros::NodeHandle  nh;
 const byte motor_pin_begin = 6;
 const byte util_pin_begin = 2;
 
+//ROS Node
+ros::NodeHandle  nh;
 
+//callback function for cmd_thruster
 void msg_Cb( const rov::cmd_thruster &msg){
-  
   for(int i =0; i< 6; i++){
-    motor[i].write(msg.thruster_val[i]*90. + 90.);
+    motor[i].writeMicroseconds(msg.thruster_val[i]*400 + 1500);
   }
   
   for(int i =0; i< 4; i++){
@@ -35,28 +54,51 @@ void msg_Cb( const rov::cmd_thruster &msg){
   }
 }
 
-geometry_msgs::Twist twist_msg;
-ros::Publisher rotationPublisher("imu_output", &twist_msg);
+// function to print the temperature for a device
+//float printTemperature(DeviceAddress deviceAddress)
+//{
+//  float tempF = sensors.getTempF(deviceAddress);
+//  if(tempF == DEVICE_DISCONNECTED_C) 
+//  {
+//    return(-255);
+//  }
+//  return(tempF);
+//}
+
+geometry_msgs::Pose pose_msg;
+//std_msgs::Int8 temp_msg;
+ros::Publisher rotationPublisher("imu_output", &pose_msg);
 ros::Subscriber<rov::cmd_thruster> sub("cmd_thruster", &msg_Cb );
+//ros::Publisher tempPublisher("temperature", &temp_msg);
 
 void setup()
 { 
+  //IMU Setup
   Serial.begin(115200);
   if (!bno08x.begin_I2C()) {
     while (1) { delay(10); }
   }
   nh.getHardware()->setBaud(115200);
-
-  bno08x.enableReport(SH2_GAME_ROTATION_VECTOR);
+  bno08x.enableReport(SH2_GYRO_INTEGRATED_RV);
   bno08x.enableReport(SH2_ACCELEROMETER);
 
+  //Temp Sensor Setup
+//  sensors.begin();
+//  sensors.getDeviceCount();
+//  sensors.getAddress(insideThermometer, 0);
+//  sensors.setResolution(insideThermometer, 9);
+//  delay(100);
+  
+  //ROS Setup
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(rotationPublisher);
-  
+//  nh.advertise(tempPublisher);
+
+  //Motor Assignments
   for(int i = 0; i<6; i++){
     motor[i].attach(i+motor_pin_begin);
-    motor[i].write(90); // 1500 for the thrusters  
+    motor[i].writeMicroseconds(1500); // 1500 for the thrusters  
   }
     for(int i = 0; i<4; i++){
       pinMode(i+util_pin_begin, OUTPUT);
@@ -66,25 +108,27 @@ void setup()
 
 void loop()
 { 
-  delay(1);
+  //delay(1);
   if (! bno08x.getSensorEvent(&sensorValue)) {
     return;
   }
-  
-  switch (sensorValue.sensorId) {
-    
-    case SH2_GAME_ROTATION_VECTOR:
-      twist_msg.angular.x = sensorValue.un.gameRotationVector.i;
-      twist_msg.angular.y = sensorValue.un.gameRotationVector.j;
-      twist_msg.angular.z = sensorValue.un.gameRotationVector.k;
+  switch (sensorValue.sensorId) {   
+    case SH2_GYRO_INTEGRATED_RV:
+      pose_msg.orientation.x = sensorValue.un.gyroIntegratedRV.i;
+      pose_msg.orientation.y = sensorValue.un.gyroIntegratedRV.j;
+      pose_msg.orientation.z = sensorValue.un.gyroIntegratedRV.k;
+      pose_msg.orientation.w = sensorValue.un.gyroIntegratedRV.real;
       break;
-
     case SH2_ACCELEROMETER:
-      twist_msg.linear.x = sensorValue.un.accelerometer.x;
-      twist_msg.linear.y = sensorValue.un.accelerometer.y;
-      twist_msg.linear.z = sensorValue.un.accelerometer.z;
+      pose_msg.position.x = sensorValue.un.accelerometer.x;
+      pose_msg.position.y = sensorValue.un.accelerometer.y;
+      pose_msg.position.z = sensorValue.un.accelerometer.z;
       break;
   }
-  rotationPublisher.publish( &twist_msg ); 
+//  sensors.requestTemperatures(); // Send the command to get temperatures
+    // It responds almost immediately. Let's print out the data
+//  temp_msg.data = printTemperature(insideThermometer);
+//  tempPublisher.publish(&temp_msg);
+  rotationPublisher.publish( &pose_msg ); 
   nh.spinOnce();
 }
