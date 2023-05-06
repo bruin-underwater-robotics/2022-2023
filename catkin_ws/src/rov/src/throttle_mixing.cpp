194c2c8cf686d43033b7062265cf922389d7df7a
AX90L0TL <38091876+ax90l0tl@users.noreply.github.com>
2023-05-06 13:35:59 -0700

    rates and expo

--- catkin_ws/src/rov/src/throttle_mixing.cpp
+++ catkin_ws/src/rov/src/throttle_mixing.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include <dynamic_reconfigure/server.h>
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/Joy.h"
 #include "rov/cmd_thruster.h"
@@ -24,15 +25,37 @@ struct Throttle_mixer{
         |    |
         3----6
         */
-        const int forward[6] = {1, 0, 1, 0, 1, 1};
-        const int right[6] = {-1, 0, 1, 0, 1, -1};
-        const int up[6] = {0, -1, 0, 1, 0, 0};
-        const int yaw[6] = {-1, 0, -1, 0, 1, 1};
-        const int roll[6] = {0, -1, 0, -1, 0, 0};
+        const int forward[6] = {1, 0, 1, 1, 0, 1};
+        const int right[6] = {-1, 0, 1, 1, 0, -1};
+        const int up[6] = {0, -1, 0, 0, 1, 0};
+        const int yaw[6] = {-1, 0, -1, 1, 0, 1};
+        const int roll[6] = {0, -1, 0, 0, -1, 0};
         float output[6] = { 0 };
         bool val[17] = { 0 };
         bool prev_state[17] = { 0 };
         double multiplier = 0.5;
+        
+        nh.param<double>("x_rate", x_rate_, 1.0);
+        nh.param<double>("y_rate", y_rate_, 1.0);
+        nh.param<double>("z_rate", z_rate_, 1.0);
+        nh.param<double>("pitch_rate", pitch_rate_, 1.0);
+        nh.param<double>("roll_rate", roll_rate_, 1.0);
+        nh.param<double>("yaw_rate", yaw_rate_, 1.0);
+        
+        nh.param<double>("x_expo", x_rate_, 1.0);
+        nh.param<double>("y_expo", y_rate_, 1.0);
+        nh.param<double>("z_expo", z_rate_, 1.0);
+        nh.param<double>("pitch_expo", pitch_rate_, 1.0);
+        nh.param<double>("roll_expo", roll_rate_, 1.0);
+        nh.param<double>("yaw_expo", yaw_rate_, 1.0);
+        
+        double rateCalc(const double rate, const double expo);
+        
+        // dynamic reconfiguration
+        dynamic_reconfigure::Server<Throttle_mixer::rateConfig> config_server;
+        dynamic_reconfigure::Server<Throttle_mixer::rateConfig>::CallbackType f;
+        f = boost::bind(&Throttle_mixer::reconfigureCallback, this, _1, _2);
+        config_server.setCallback(f);
 };
 
 Throttle_mixer::Throttle_mixer(){
@@ -76,14 +99,47 @@ void Throttle_mixer::joy_callBack(const sensor_msgs::Joy::ConstPtr& joy_msg){
     cmd_thruster_pub.publish(msg);
 }
 
+double Throttle_mixer::rateCalc(const double input, const double rate, const double expo){
+        if(input < 0){
+                y = rate*pow(input, expo);
+        }else{
+                y = -rate*pow(input, expo);
+        }
+        return y;
+}
+
+void Throttle_mixer::reconfigureCallback(Throttle_mixer::Config& config, uint32_t level)
+{
+  if (first_reconfig_)
+  {
+    getParam("x_rate", x_rate_);
+    getParam("x_expo", x_expo_);
+    getParam("y_rate", y_rate_);
+    getParam("y_expo", y_expo_);
+    getParam("z_rate", z_rate_);
+    getParam("z_expo", z_expo_);
+    first_reconfig_ = false;
+    return;  // Ignore the first call to reconfigure which happens at startup
+  }
+  ROS_INFO("Pid reconfigure request: Kp: %f, Ki: %f, Kd: %f", Kp_, Ki_, Kd_);
+}
+
 void Throttle_mixer::cmd_velCallback(const geometry_msgs::Twist::ConstPtr& twist_msg){
-    
     msg.thruster_val.clear();
+        
+    x_val = (twist_msg->linear.x, nh.getParam("x_rate", x_rate_), nh.getParam("x_expo", x_expo_));
+    y_val = (twist_msg->linear.y, nh.getParam("y_rate", y_rate_), nh.getParam("y_expo", y_expo_));
+    z_val = (twist_msg->linear.z, nh.getParam("z_rate", z_rate_), nh.getParam("z_expo", z_expo_));
+    //pitch_val = (twist_msg->angular.x, nh.getParam("x_rate", x_rate_), nh.getParam("x_expo", pitch_expo_));
+    roll_vall = (twist_msg->angular.y, nh.getParam("roll_rate", roll_rate_), nh.getParam("roll_expo", roll_expo_));
+    yaw_val = (twist_msg->angular.z, nh.getParam("yaw_rate", yaw_rate_), nh.getParam("yaw_expo", yaw_expo_));
+    
+        
     for(int i =0; i<6; i++){
-        output[i] = twist_msg->linear.x*forward[i] + twist_msg->linear.y*right[i] + 
-        twist_msg->linear.z*up[i] + twist_msg->angular.z*yaw[i] + twist_msg->angular.x*roll[i];
+        output[i] = x_val*forward[i] + y_val*right[i] + 
+        z_val*up[i] + yaw_val*yaw[i] + roll_val*roll[i];
         if(output[i] != 0){
-            output[i] = multiplier*output[i]/(ceil(abs(twist_msg->linear.x*forward[i])) + ceil(abs(twist_msg->linear.y*right[i])) + ceil(abs(up[i])) + ceil(abs(twist_msg->angular.z*yaw[i])) + ceil(abs(twist_msg->angular.x*roll[i])));      
+            output[i] = multiplier*output[i]/(ceil(abs(x_val*forward[i])) + ceil(abs(y_val*right[i])) + ceil(abs(z_val*up[i])) + ceil(abs(yaw_val*yaw[i])) + ceil(abs(roll_val*roll[i])));      
         }
         msg.thruster_val.push_back(output[i]);
     }
